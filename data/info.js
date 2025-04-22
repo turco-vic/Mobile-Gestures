5209303e7db864fcfc36676daa4735fdcab37a8a
Enzo Alves Turcovic <enzoturcovic@gmail.com>
2025-04-22 13:41:11 -0300

    Create data/info.js

---
+++ data/info.js
@@ -0,0 +1,22 @@
+export const memes = [
+    {
+        id: 1,
+        name: "Melzinha Zoiuda",
+        url: "https://wallpapers.com/images/featured/imagem-engracada-de-cachorro-f0sbo1li5pvc91oj.jpg",
+    },
+    {
+        id: 2,
+        name: "Esquilo da Oração",
+        url: "https://i.natgeofe.com/n/c0e0a134-3e97-4b8f-9f7b-9d11f5e1bf02/comedy-wildlife-awards-squirel-stop.jpg"
+    },
+    {
+        id: 3,
+        name: "Masqueico do grito",
+        url: "https://imageresizer.static9.net.au/3RNEBTwCbyXbYzjLHA3fOBLq-iU=/400x0/https%3A%2F%2Fprod.static9.net.au%2Ffs%2F646e6914-cc2a-452e-82ce-b40120e6b82f"
+    },
+    {
+        id: 4,
+        name: "Rei Julian",
+        url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSx3Sl-jO8MQgoxOQoQ5jr50zF9QAes9xS_NA&s"
+    },
+];
